8f00a10bb08cdbc0ee13b3668b78848a4b1293a9
allan mugenyi <157706090+ah-lan@users.noreply.github.com>
2024-07-19 18:06:43 +0300

    Update funds.dart

--- lib/views/categories/funds.dart
+++ lib/views/categories/funds.dart
@@ -19,3 +19,30 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class FundsPage extends StatefulWidget {
+  @override
+  _FundsPageState createState() => _FundsPageState();
+}
+
+class _FundsPageState extends State<FundsPage> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _amountController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _messageController = TextEditingController();
+
+  List<double> _mostDonatedAmounts = [10000.0, 20000.0, 50000.0, 100000.0];
+  String _selectedCurrency = 'UGX'; // Default currency
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Fund Donation'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
