6b4ac47679b7c359e866e42db49a3845976ce4ce
muwanguzimercy <mercymuwanguzi23@gmail.com>
2024-07-20 11:26:11 +0300

    Add files via upload
    confirmation page after donating

---
+++ lib/views/categories/FOOD/confirmation_page.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+import 'package:sharecare/views/tracker/pickup1.dart';
+
+class ConfirmationPage extends StatelessWidget {
+  final String? selectedSource;
+  final String? foodType;
+  final String? vehicleType;
+  final int quantity;
+  final bool isAnonymous;
+
+  const ConfirmationPage({
+    super.key,
+    required this.selectedSource,
+    required this.foodType,
+    required this.vehicleType,
+    required this.quantity,
+    required this.isAnonymous,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black54, // to simulate a modal overlay
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            color: Colors.teal,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          width: MediaQuery.of(context).size.width * 0.8,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              _buildHeader(context),
+              const SizedBox(height: 20),
+              _buildConfirmationDetails(),
+              const SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context); // Go back to the form page
+                    },
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.teal,
+                      backgroundColor: Colors.white,
+                      side: const BorderSide(color: Colors.tealAccent),
+                    ),
+                    child: const Text('Back'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                const SetPickupLocationPage()),
+                      );
+                      // Handle confirmation action here (e.g., submit data)
+                      // ...
+                    },
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.teal,
+                    ),
+                    child: const Text('Proceed to Pickup'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHeader(BuildContext context) {
+    return Stack(
+      children: [
+        const Center(
+          child: Column(
+            children: [
+              Icon(
+                Icons.assignment_turned_in,
+                color: Colors.white,
+                size: 40,
+              ),
+              SizedBox(height: 8),
+              Text(
+                'Please Confirm',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          right: 0,
+          top: 0,
+          child: IconButton(
+            icon: const Icon(Icons.close, color: Colors.white),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildConfirmationDetails() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _buildDetailRow('Donation Category', 'Food'),
+        _buildDetailRow('Food Type', foodType),
+        _buildDetailRow('Source of Food', selectedSource),
+        _buildDetailRow('Food Quantity', '$quantity people Approx.'),
+        _buildDetailRow('Charity Organization', 'Goodwill Charity'),
+        _buildDetailRow('Vehicle Type', vehicleType),
+        _buildDetailRow('You have chosen to donate anonymously',
+            isAnonymous ? 'Yes' : 'No'),
+      ],
+    );
+  }
+
+  Widget _buildDetailRow(String label, String? value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            label,
+            style: const TextStyle(fontSize: 18, color: Colors.white),
+          ),
+          Text(value ?? '',
+              style: const TextStyle(fontSize: 18, color: Colors.white),
+              overflow: TextOverflow.ellipsis),
+        ],
+      ),
+    );
+  }
+}
