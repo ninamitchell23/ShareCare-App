049e1f6493c4eb5d5735dc4fec5788332600cff3
ninamitchell23 <157416946+ninamitchell23@users.noreply.github.com>
2024-07-25 08:01:17 +0300

    Create banner_button.dart

---
+++ lib/views/Donate/banner_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+
+class CustomButton extends StatelessWidget {
+  final String? text;
+  final VoidCallback? onPressed;
+  final Color? color;
+  final Color? textColor;
+
+  const CustomButton({
+    Key? key,
+    this.text,
+    this.onPressed,
+    this.color,
+    this.textColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.all(16),
+        iconColor: Colors.blue,
+      ),
+      child: const Text("View Details"),
+    );
+  }
+}
