5925cdbe1694669d59def8aeb939d919f63d40df
allan mugenyi <157706090+ah-lan@users.noreply.github.com>
2024-08-12 12:24:57 +0300

    Create constants.dart

---
+++ lib/views/categories/paypal/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static const String clientId ='AWtbSUXAMotjKGmoivaoAxJTfWfYyrNaPp5Nh7nCKvrwJVD_Rp0ndQkbmmWmubqqSFJUhESBFUuCklAC';
+  static const String secretKey ='EGvAdcL4gDFPcRDljw4ryvSKQvwe6sSodKksJQfr3YikyO9KMPjXwCdFj0-NRAaJzr2yiO9QBCitISBr';
+  static const String returnURL ='https://samplesite.com/return';
+  static const String cancelURL ='https://samplesite.com/cancel';
+}
